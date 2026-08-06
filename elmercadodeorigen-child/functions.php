c0a989f49e8983e87b0e4fc47a55a89a1be33944
tostis005 <tostis005@gmail.com>
2026-08-06 15:48:14 +0200

    Load storefront edge fixes and bump theme version

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.9.3' );
+define( 'ELMERCADO_THEME_VERSION', '0.9.4' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -47,6 +47,7 @@ $elmercado_modules = array(
 	'inc/storefront-final-pass.php',
 	'inc/premium-storefront-polish.php',
 	'inc/visual-correction-093.php',
+	'inc/storefront-edge-fix.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
