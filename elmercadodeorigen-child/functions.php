493bc122d2d440ce14ec4cc535a4ca81f7c01fa8
tostis005 <tostis005@gmail.com>
2026-08-06 07:04:32 +0200

    chore: release editorial version 0.6.2

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.6.1' );
+define( 'ELMERCADO_THEME_VERSION', '0.6.2' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
