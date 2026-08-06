6bda5d4f261a65741bb0b2527076b2e82674ab5f
tostis005 <tostis005@gmail.com>
2026-08-06 06:44:32 +0200

    chore: load editorial release

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.5.1' );
+define( 'ELMERCADO_THEME_VERSION', '0.6.0' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -26,6 +26,7 @@ require_once ELMERCADO_THEME_PATH . '/inc/home-refresh.php';
 require_once ELMERCADO_THEME_PATH . '/inc/home-header-normalize.php';
 require_once ELMERCADO_THEME_PATH . '/inc/release-one.php';
 require_once ELMERCADO_THEME_PATH . '/inc/release-one-finish.php';
+require_once ELMERCADO_THEME_PATH . '/inc/editorial-system.php';
 
 /* La optimización se ejecuta una sola vez, al final del encolado normal. */
 remove_action( 'wp_print_styles', 'elmercado_optimize_home_assets', 0 );
