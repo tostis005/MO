ff5347bbf0d47e2e2a59725f31c07f6ed63c5c7b
tostis005 <tostis005@gmail.com>
2026-08-07 16:38:09 +0200

    Bump theme to 0.10.39 and load responsive layout refinement

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.38' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.39' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -71,6 +71,7 @@ $elmercado_modules = array(
 	'inc/checkout-contrast-final-01035.php',
 	'inc/checkout-stock-final-01036.php',
 	'inc/store-vendor-layout-final-01037.php',
+	'inc/layout-density-final-01039.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
