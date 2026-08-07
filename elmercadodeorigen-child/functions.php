9ecd4c7e3ce2c2a457a03632494b5e7a48429cff
tostis005 <tostis005@gmail.com>
2026-08-07 10:52:18 +0200

    Bump child theme to 0.10.17

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.16' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.17' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -58,6 +58,7 @@ $elmercado_modules = array(
 	'inc/runtime-stability-final.php',
 	'inc/vendor-toolbar-mobile-final.php',
 	'inc/storefront-second-review-final.php',
+	'inc/cart-counter-visibility-final.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
