f52125f7c25aaaf8259fed7f9413c07511076407
tostis005 <tostis005@gmail.com>
2026-08-07 08:46:26 +0200

    Bump child theme to 0.10.14

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.13' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.14' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
