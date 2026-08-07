8f58d16fbb7cc39736b5331eaa520fdb64d5ca8d
tostis005 <tostis005@gmail.com>
2026-08-07 14:49:35 +0200

    Bump theme to 0.10.34

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.33' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.34' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
