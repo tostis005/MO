d9c9f859ee912e8b3c07891f66c3b3ef2a7bc6c0
tostis005 <tostis005@gmail.com>
2026-08-10 17:56:47 +0200

    Remove Home diagnostic and bump to 0.10.151

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.150' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.151' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -126,7 +126,6 @@ $elmercado_modules = array(
 	'inc/home-performance-second-pass-010147.php',
 	'inc/home-inline-jquery-010148.php',
 	'inc/home-voice-performance-final-010149.php',
-	'inc/home-inline-diagnostic-010150.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
