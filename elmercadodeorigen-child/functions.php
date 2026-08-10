9e36e50ba99c3f0b6482aa2ff1092c56ad2fdfa6
tostis005 <tostis005@gmail.com>
2026-08-10 18:21:13 +0200

    Load clean Home copy in 0.10.152

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.151' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.152' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -126,6 +126,7 @@ $elmercado_modules = array(
 	'inc/home-performance-second-pass-010147.php',
 	'inc/home-inline-jquery-010148.php',
 	'inc/home-voice-performance-final-010149.php',
+	'inc/home-copy-clean-final-010152.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
