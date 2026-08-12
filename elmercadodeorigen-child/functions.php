c3ddf706ab3851d4d482ff9183c08aa77c112b6e
tostis005 <tostis005@gmail.com>
2026-08-12 23:30:32 +0200

    Release Home category edge alignment 0.10.215

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.214' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.215' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -141,6 +141,7 @@ $elmercado_modules = array(
 	'inc/wcfm-disabled-vendor-ui-010211.php',
 	'inc/mobile-price-slider-alignment-010209.php',
 	'inc/home-category-visibility-010212.php',
+	'inc/home-category-edge-align-010215.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
