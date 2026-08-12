b150f24b2fe0392987e7b024cbeaca4e48b7cae6
tostis005 <tostis005@gmail.com>
2026-08-12 14:56:55 +0200

    Activate unified filter system 0.10.195

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.194' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.195' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -145,6 +145,7 @@ $elmercado_modules = array(
 	'inc/cart-shipping-copy-final-010192.php',
 	'inc/catalog-core-filters-010193.php',
 	'inc/vendor-filter-visual-fix-010194.php',
+	'inc/catalog-filter-unification-010195.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
