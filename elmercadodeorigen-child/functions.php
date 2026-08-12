a1d0564cb9f3fbf20f34f7c7893cea55e7f71624
tostis005 <tostis005@gmail.com>
2026-08-12 21:14:43 +0200

    Activate disabled WCFM vendor visibility guard 0.10.210

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.209' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.210' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -136,6 +136,7 @@ $elmercado_modules = array(
 	'inc/cart-checkout-contact-cleanup-010185.php',
 	'inc/category-specific-filters-010185.php',
 	'inc/cart-shipping-copy-final-010192.php',
+	'inc/wcfm-disabled-vendor-visibility-010210.php',
 	'inc/catalog-filter-system-final-010207.php',
 	'inc/mobile-price-slider-alignment-010209.php',
 );
