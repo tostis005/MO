3ffeef6000a221eae863a4c1c725235f00cdf809
tostis005 <tostis005@gmail.com>
2026-08-13 07:45:24 +0200

    Release single visible result counter 0.10.221

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'ELMERCADO_THEME_VERSION', '0.10.220' );
+define( 'ELMERCADO_THEME_VERSION', '0.10.221' );
 define( 'ELMERCADO_THEME_PATH', get_stylesheet_directory() );
 define( 'ELMERCADO_THEME_URL', get_stylesheet_directory_uri() );
 
@@ -144,6 +144,7 @@ $elmercado_modules = array(
 	'inc/home-category-edge-align-010215.php',
 	'inc/catalog-visibility-counts-010217.php',
 	'inc/catalog-result-total-010220.php',
+	'inc/catalog-result-total-cleanup-010221.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
