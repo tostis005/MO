4e54ab137ad8c79b7f215809a12b072255d7fbcc
tostis005 <tostis005@gmail.com>
2026-08-13 15:58:15 +0200

    Load Home category truth before legacy visibility pass 0.10.226

--- elmercadodeorigen-child/functions.php
+++ elmercadodeorigen-child/functions.php
@@ -148,6 +148,7 @@ $elmercado_modules = array(
 	'inc/catalog-toolbar-mobile-price-fix-010222.php',
 	'inc/catalog-query-parity-010224.php',
 	'inc/vendor-store-catalog-filters-010225.php',
+	'inc/home-category-truth-before-legacy-010226.php',
 );
 
 foreach ( $elmercado_modules as $elmercado_module ) {
