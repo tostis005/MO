569044957446fe40e04791436ee65bc02082aef1
tostis005 <tostis005@gmail.com>
2026-09-17 10:32:31 +0200

    Always show EMDO store review form

--- mdo-supplier-sync/templates/store/wcfmmp-view-store-mdo-reviews.php
+++ mdo-supplier-sync/templates/store/wcfmmp-view-store-mdo-reviews.php
@@ -7,3 +7,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( class_exists( 'MDO_Reviews_Public' ) ) {
 	MDO_Reviews_Public::render_current_store();
 }
+
+if ( class_exists( 'MDO_Reviews_Submission' ) ) {
+	$store_url = (string) get_option( 'wcfm_store_url', 'store' );
+	$store_name = apply_filters( 'wcfmmp_store_query_var', get_query_var( $store_url ) );
+	$store_user = is_string( $store_name ) && '' !== $store_name ? get_user_by( 'slug', $store_name ) : false;
+	if ( $store_user ) {
+		MDO_Reviews_Submission::render_form( (int) $store_user->ID );
+	}
+}
